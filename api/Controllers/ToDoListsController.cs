c8f1ffecd25496dda93046ade92bb9c29fee3506
Neo See <neococo123@yahoo.com>
2024-05-02 20:58:57 -0700

    Create and executed CRUD

--- api/Controllers/ToDoListsController.cs
+++ api/Controllers/ToDoListsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using api.Data;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -10,6 +13,67 @@ namespace api.Controllers
     [Route("api/[controller]")]
     public class ToDoListsController : ControllerBase
     {
-        
+        private readonly AppDbContext _context;
+
+        public ToDoListsController(AppDbContext context){
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<IEnumerable<ToDoList>> getList(){
+            var toDoList = await _context.ToDoLists.AsNoTracking().ToListAsync();
+            return toDoList;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(ToDoList ToDoList){
+            if(!ModelState.IsValid){
+                return BadRequest("(ノಠ益ಠ)ノ彡┻━┻");
+            }
+            await _context.AddAsync(ToDoList);
+
+            var result = await _context.SaveChangesAsync();
+            if(result > 0){
+                return Ok();
+            }
+            return BadRequest("(ノಠ益ಠ)ノ彡┻━┻");
+        }
+
+        [HttpPut("{id:int}")]
+
+        public async Task<IActionResult> EditList(int id, ToDoList ToDoList){
+            var taskFromDb = await _context.ToDoLists.FindAsync(id);
+            if(taskFromDb == null){
+                return BadRequest("Task Not Found (ノಠ益ಠ)ノ彡┻━┻");
+            }
+
+            taskFromDb.Description = ToDoList.Description;
+
+            var result = await _context.SaveChangesAsync();
+            if(result > 0){
+                return Ok();
+            }
+            return BadRequest("(ノಠ益ಠ)ノ彡┻━┻");
+        }
+
+        [HttpDelete("(id:int)")]
+
+        public async Task<IActionResult> Delete(int id){
+            var toDoList = await _context.ToDoLists.FindAsync(id);
+
+            if(toDoList == null){
+                return NotFound("Task was not found (ノಠ益ಠ)ノ彡┻━┻");
+            }
+
+            _context.Remove(toDoList);
+
+            var result = await _context.SaveChangesAsync();
+            if(result > 0){
+                return Ok();
+            }
+            return BadRequest("Unable to delete task (ノಠ益ಠ)ノ彡┻━┻");
+        }
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ceaa5d3479db07b911a9f7019018fd7e171d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+449e5a8d7c0b2347631cc7f85a258e02dbacc033")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
